76f6d2f2679da72f9ede39d77875b15beba7f5c8
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-05-01 21:10:40 -0500

    newChanges to ClassWork

--- ConsoleAppDay2/ConsoleAppDay2/Program.cs
+++ ConsoleAppDay2/ConsoleAppDay2/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* multipl.
+int numb = 3, numb2 = 4;
+
+Console.WriteLine(numb / numb2);
+
+Console.WriteLine(numb % numb2);*/
+
+/*increment
+
+int numb = 1;
+Console.WriteLine(++numb);
+Console.WriteLine(numb);
+
+int numb2 = 2;
+Console.WriteLine(numb2++);
+Console.WriteLine(numb2);
+
+//decrem
+
+int numb3 = 3;
+Console.WriteLine(--numb3);
+Console.WriteLine(numb3);
+int numb4 = 4;
+Console.WriteLine(numb4--);
+Console.WriteLine(numb4);*/
+
+//compar
+
+int numb = 12, numb2 = 13;
+
+bool result = numb >= numb2;
+bool result2 = numb < numb2;
+
+Console.WriteLine($" {result} and {result2}");
+
+
+
+
