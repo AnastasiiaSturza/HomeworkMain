85289da9375e054164b74b37f2bdddf9a411388e
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-05-01 20:06:56 -0500

    add visCode

---
+++ ConsoleAppTestDay1/ConsoleAppTestDay1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Add changes");
+
+Console.WriteLine("Hello Anastasiia");
+
+Console.WriteLine("Day1 new");
