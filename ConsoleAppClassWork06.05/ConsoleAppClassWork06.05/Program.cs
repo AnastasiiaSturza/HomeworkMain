25388dc3c20ae06c2acf026f8a086e1424009f2a
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-05-08 22:55:06 -0500

    classwork

---
+++ ConsoleAppClassWork06.05/ConsoleAppClassWork06.05/Program.cs
@@ -0,0 +1,14 @@
+﻿
+int SumOfnumbersRec(int a, int b )
+{
+    int result = 0; 
+    if( a <= b )
+    {
+        result += a + SumOfnumbersRec(a + 1, b);
+    }
+    /*else
+    {
+        result += b + SumOfnumbersRec(a, b + 1);
+    }*/
+    return result;
+}
