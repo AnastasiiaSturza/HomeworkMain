bb11919b181e259c6b37fac674372eb8ec3d6073
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-05-20 10:34:41 -0500

    classWork20.05

---
+++ ConsoleApp20.05/ConsoleApp20.05/Cathedra.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20._05
+{
+    public class Cathedra
+    {
+        public string Name { get; set; }
+        public Group[] Groups { get; set; }
+
+        public Teacher[] Teachers { get; set; }
+         public Student[] Students { get; set; }
+        public Lesson[] Lessons { get; set; }
+    }
+
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public Lesson[] Lessons { get; set; }
+    }
+
+    public class Group
+     {
+        public string Name { get; set; }
+        public Student[] Students { get; set; }
+        public Cathedra[] Cathedras { get; set; }
+        public Lesson[] Lessons { get; set; }
+        // set cathedra  + add/ remove students/teachers + add/remove lessons
+
+     }
+    
+    public class Student
+    {
+        public string Name { get; set; }
+        public Group Group { get; set; }
+    }
+   
+    public class Lesson
+    {
+        public string Name { get; set; }
+        public Student[] Students { get;set; }
+        public Teacher Teacher { get; set; }
+        public Room Room { get; set; }
+    }
+
+    public class Room
+    {
+        public Teacher Teacher { get; set;}
+        public Lesson Lesson { get; set;}
+
+    }
+}
+
---
+++ ConsoleApp20.05/ConsoleApp20.05/Program.cs
@@ -0,0 +1,41 @@
+﻿// student , teachers,  rooms, lessons
+
+using ConsoleApp20._05;
+
+var cathedra = new Cathedra
+{
+    Name = "Tech Cathedra",
+    Teachers = new[]
+    {
+        new Teacher
+        {
+            Name = "John"
+        },
+        new Teacher
+        {
+            Name= "Marry"
+        }
+    },
+    Groups = new[]
+    {
+        new Group
+        {
+            Name = "a1"
+        }
+    },
+    Students = new[]
+    {
+        new Student
+        {
+
+        }
+    },
+    Lessons = new[]
+    {
+        new Lesson
+        {
+            Name = "Math"
+        }
+    }
+};
+Console.WriteLine(cathedra);
