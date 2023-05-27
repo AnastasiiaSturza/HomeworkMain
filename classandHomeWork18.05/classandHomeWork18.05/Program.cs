d0db85d273434b2d69fd073a183032a81ee43732
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-05-27 01:39:22 -0500

    ClassWork18.05

---
+++ classandHomeWork18.05/classandHomeWork18.05/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using classandHomeWork18._05;
+
+var person = new Person("Artem","Sokolov","Odessa", 27, "123456");
+var anotherPerson = new Person("Jonh", "Doe", "Chicago", 42, "789123");
+var copyPerson = new Person(anotherPerson);
+anotherPerson.Age = 30;
+Console.WriteLine(anotherPerson.Age);
+anotherPerson.FullName();
+var info = person.GetInfo();
+var phoneBook = new PhoneBook();
+phoneBook.addNewRecord(person);
+phoneBook.addNewRecord(anotherPerson);
+phoneBook.addNewRecord(copyPerson);
+
+phoneBook.ShowAll();
+
+
+Console.WriteLine(info);
+Console.ReadLine();
---
+++ classandHomeWork18.05/classandHomeWork18.05/person.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classandHomeWork18._05
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public int Age { get; set; }
+        
+        public string PhoneNumber { get; set; }
+
+
+        public Person(string firstName, string lastName , string adress , int age, string phoneNumber) 
+        { 
+            FirstName = firstName;
+            LastName = lastName;
+            Adress = adress;
+            Age = age;
+            PhoneNumber = phoneNumber;
+
+        }
+
+
+        public Person(Person person) 
+        {
+            FirstName = person.FirstName;
+            LastName = person.LastName;
+            Adress = person.Adress;
+            Age = person.Age;
+            PhoneNumber = person.PhoneNumber;
+        }
+
+        public void FullName()
+        {
+            Console.WriteLine($"{FirstName} {LastName}");
+        }
+
+        public string GetInfo()
+        {
+            return $"{FirstName} {LastName} adress : {Adress} and age : {Age}";
+        }
+    }
+}
