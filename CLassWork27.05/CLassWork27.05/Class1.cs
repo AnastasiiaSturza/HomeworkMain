40874beddb3700dee130217cd235009e2908c91c
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-05-28 13:38:24 -0500

    classwork27.05

---
+++ CLassWork27.05/CLassWork27.05/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLassWork27._05
+{
+    public struct Point
+    {
+        int X;
+        int Y;
+        int Z;     // + object C
+
+        public Point(int x, int y, int z)  //+ object c
+        {
+            this.X = x; this.Y = y; this.Z = z;
+        }
+
+        public static Point operator + (Point first, Point second)
+        {
+            return new Point(first.X + second.X, first.Y + first.X, second.X + second.Y);
+        }
+       
+    }
+}
---
+++ CLassWork27.05/CLassWork27.05/Program.cs
@@ -0,0 +1,35 @@
+﻿/*using CLassWork27;
+using CLassWork27._05;
+
+var structure = new Point(10, 12, 0);
+var strucure2 = new Point(10, 12, 10);
+var structure3 = structure + strucure2;
+
+
+var user = new User
+{
+    Name = "Test",
+
+    Password = "123354"
+};
+
+var userType = user.GetType();
+object userObject = (object)user;
+
+var userObjecttype = userObject.GetType();
+
+//var userFromObject = (User)userObject;
+//var name = userFromObject.Name;
+int i = 100;
+long j = 100;
+object intObj = (object)i;
+int intFromintObj = (int)intObj;
+
+object longObj = (object)j;
+int intFromLongObj = (int)longObj;
+
+
+ Console.ReadLine();*/
+
+
+
