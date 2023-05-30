7deac6bcfcd7976e057f6ea603f94d3f5ef393f6
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-05-30 17:13:08 -0500

    classWork30.05

---
+++ ConsoleAppClassWork30.05/ConsoleAppClassWork30.05/LinkedList.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClassWork30._05
+{
+    public class LinkedList <T> //add , get(index), clear element
+    {
+        private Item head;  // first eement in collection
+
+       /* public void Insert(T element, int index)
+        {
+            if (index < 1)
+            {
+                throw new InvalidOperationException();
+            }
+            if (head == null)
+            {
+                throw new InvalidOperationException();
+            }
+            if (index == 1)
+            {
+                return head.Value;
+            }
+            var currentItem = head;
+            int i = 1;
+            while (i++ < index && currentItem != null)
+            {
+                currentItem = currentItem.Next;
+            }
+            if (currentItem == null)
+            {
+                throw new ArgumentException("Such index doesn't exist");
+            }
+            return currentItem.Value;
+        
+        }*/
+
+        public int Count()
+            
+        {
+            if (head == null)
+            {
+                return 0;
+            }
+            var currentItem = head;
+            int i =1;
+            while (currentItem != null)
+            {
+                i++;
+                currentItem = currentItem.Next;
+            }
+            return i;
+        }
+        public void Add(T element)
+        {
+            if (head == null)
+            {
+                head = new Item()
+                {
+                    Value = element,
+                    Next = null
+
+                };
+            }
+            else
+            {
+                var nextElement = head.Next;
+                var currentElement = head;
+                {
+                    while (nextElement != null)
+                    {
+                        currentElement = nextElement;
+                        nextElement = nextElement.Next;
+                    }
+                }
+                var newItem = new Item()
+                {
+                    Value = element,
+
+                    Next = null
+                };
+                currentElement = newItem;
+            }
+        }
+
+        public T Get(int index)
+        {
+            if (index < 1)
+            {
+                throw new InvalidOperationException();
+            }
+            if (head == null)
+            {
+                throw new InvalidOperationException();
+            }
+            if (index == 1)
+            {
+                return head.Value;
+            }
+            var currentItem = head;
+            int i = 1;
+            while (i++ < index && currentItem != null)
+            {
+                currentItem = currentItem.Next;
+            }
+            if (currentItem == null)
+            {
+                throw new ArgumentException("Such index doesn't exist");
+            }
+            return currentItem.Value;
+        }
+        public void Clear()
+        {
+            head = null;
+        }
+
+
+
+
+        private class Item 
+        { 
+            public T Value { get; set; }
+            public Item Next { get; set; }
+        }
+
+    }
+}
---
+++ ConsoleAppClassWork30.05/ConsoleAppClassWork30.05/Program.cs
@@ -0,0 +1,34 @@
+﻿/*int first = 10, second = 20;
+string str = "123", str2 = "321";
+
+void Swap<T>(ref T param1, ref T param2) // for "int" use ref !!! + can use Swap<T1, T2>
+{
+    T temp = param1;
+    param1 = param2;
+    param2 = temp;
+}
+Swap(ref first , ref second);
+Swap(ref str, ref str2);
+Console.WriteLine($"first = {first}, second = {second}");
+Console.WriteLine($"str = {str}, str2 = {str2}");
+
+
+string str3 = "12345", str4 = "54321";
+string Print<U1 , U2>(ref U1 param1, ref U2 param2) 
+    where U1 : class
+    where U2 : class
+{
+    return param1.ToString() + param2.ToString();
+}
+Console.WriteLine(Print(ref str3, ref str4));*/
+var strings = new ConsoleAppClassWork30._05.LinkedList<string>();
+strings.Add("1");
+strings.Add("2");
+strings.Add("3");
+strings.Add("4");
+strings.Add("5");
+strings.Clear();
+
+var foundElement = strings.Get(4);
+
+Console.WriteLine(foundElement);
