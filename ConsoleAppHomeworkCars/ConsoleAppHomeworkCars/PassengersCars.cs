ccb294cb7ba42f558f0e8c498aa25f1cd39ca215
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-06-03 04:35:50 -0500

    Homework for 23.05

---
+++ ConsoleAppHomeworkCars/ConsoleAppHomeworkCars/PassengersCars.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppHomeworkCars
+{
+    public class PassengersCars : CarsMain
+    {
+       
+        public PassengersCars() { }
+        public  PassengersCars(string name, string color, string type, double engine, int doors)
+        {
+            Name = name;
+            Color = color;
+            Type = type;
+            Engine = engine;
+            Doors = doors;
+            
+        }
+
+        public string About()
+        {
+            return $"This is passenger car ";
+        }
+
+        public string AboutModel()
+        {
+            bool istrue = true;
+
+            return $"It is new model";
+        }
+        public override string Discription()
+        {
+            return $"This is a PASSENGER car , it  is {Name}, it type is {Type} with engine {Engine}, color is {Color} " +
+                $"and it has {Doors} doors. And it is new model ";
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is PassengersCars car)
+            //return base.Equals(obj);
+            {
+                return Name == car.Name && Color == car.Color && Engine == car.Engine;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Color, Engine);
+        }
+
+
+    }
+    public class ElectroCars : PassengersCars
+    {
+        public ElectroCars() { }
+        public ElectroCars(string name, string color, string type, double engine, int doors)
+        {
+            Name= name; Color = color; Type = type; Engine = engine; Doors = doors; 
+        }
+
+
+        public string AboutHybryd()
+        {
+            bool istrue = true;
+
+            return $"It is hybryd";
+        }
+        public override string Discription()
+        {
+            return $"This is a PASSENGER Electro car , it  is {Name}, it type is {Type} with ELECTRIC engine ," +
+                $" color is {Color} and it has {Doors} doors.";
+        }
+        
+
+
+    }
+}
---
+++ ConsoleAppHomeworkCars/ConsoleAppHomeworkCars/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+using ConsoleAppHomeworkCars;
+
+var car = new CarsMain();
+car.Name = "Car";
+car.Doors = 4;
+car.Engine = 2.5;
+car.Color = "green";
+car.Type = "sport";
+Console.WriteLine(car.About());
+Console.WriteLine(car.Discription());
+Console.WriteLine();
+
+var passCar = new PassengersCars();
+passCar.Name = "Honda";
+passCar.Doors = 4;
+passCar.Engine = 1.6;
+passCar.Color = "red";
+passCar.Type = "sedan";
+
+var passCar2 = new PassengersCars();
+passCar2.Name = "Honda Accord";
+passCar2.Doors = 3;
+passCar2.Engine = 1.6;
+passCar2.Color = "red";
+passCar2.Type = "sedan";
+
+
+Console.WriteLine(passCar.About());
+Console.WriteLine(passCar.Discription());
+Console.WriteLine();
+
+var electroCar = new ElectroCars();
+electroCar.Name = "Hundai";
+electroCar.Doors = 4;
+electroCar.Engine = 2.5;
+electroCar.Color = "blue";
+electroCar.Type = "sedan";
+
+Console.WriteLine(electroCar.About());
+Console.WriteLine(electroCar.Discription());
+
+var passCarEquals = passCar.Equals(passCar2);
+Console.WriteLine();
+
+Console.WriteLine($"PassCars  equals = {passCarEquals}");
+
+var passCarGetHashCode = passCar2.GetHashCode;
+Console.WriteLine(passCarGetHashCode());
+
+
