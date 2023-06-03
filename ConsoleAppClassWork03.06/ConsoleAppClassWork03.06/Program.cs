8ef1da4bb12b1325830bdcdda180a38c8ed88c91
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-06-03 03:06:35 -0500

    ClassWork03.06

---
+++ ConsoleAppClassWork03.06/ConsoleAppClassWork03.06/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleAppClassWork03._06;
+void Increment ()
+{
+    Console.WriteLine("Increment");
+}
+
+void Decrement ()
+{
+    Console.WriteLine("Decrement");
+}
+
+var counter = new Counter ();
+counter.IncrementAction += Increment;
+counter.DecrementAction += Decrement;
+counter.Increment ();
+counter.Decrement ();
+
+void ChangeValue ()
+{
+    Console.WriteLine();
+}
+
+
+//void Increment => Console.WriteLine();   this method name is lambda ( same with method Increment/ Decrement, what we use before)
+// void Decrement => Console.WriteLine();
---
+++ ConsoleAppClassWork03.06/ConsoleAppClassWork03.06/Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClassWork03._06
+{
+    public delegate void Increment ();
+    public delegate void Decrement ();
+    public class Counter
+    {
+        
+        public int _number { get; set; }
+
+        public delegate void ChangeValue ();
+        public Increment IncrementAction { get; set; }
+        public Decrement DecrementAction { get; set; }
+
+        public event Increment IncrementEvent;
+        public event Decrement DecrementEvent;
+        public event ChangeValue ChangeValueEvent;
+
+        public void Increment()
+        {
+            IncrementAction();
+            _number++;
+        }
+
+        public void Decrement()
+        {
+            DecrementAction();
+            _number--;
+        }
+    }
+}
