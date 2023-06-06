3e129a4a6aa93624c37c421849303fd47623c1c7
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-06-05 22:01:10 -0500

    classWork25.05

---
+++ ConsoleAppClassWork25.05/ConsoleAppClassWork25.05/DocGeneration.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClassWork25._05
+{
+    public abstract class DocGeneration
+    {
+        public abstract void DocGenerat(string data);   // abstact method , withouut {}
+
+    }
+
+    public class PdfGenerator : DocGeneration
+    {
+        public override void DocGenerat(string data)   // override from abstract method
+        {
+            //throw new NotImplementedException();
+        }
+       
+    }
+    interface Something
+    {
+        void saySmth(string s)  // void saySmth();
+        {
+            Console.WriteLine("say");
+        }
+    }
+
+    interface ICanDoSomethng : Something
+    {
+        void CanDoSomething(bool irTrue)
+        {
+            Console.WriteLine("True");
+        }
+    }
+
+    public class NewGenerator : DocGeneration, Something , ICanDoSomethng
+    {
+        public override void DocGenerat(string data)
+        {
+           // throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ ConsoleAppClassWork25.05/ConsoleAppClassWork25.05/Program.cs
@@ -0,0 +1,7 @@
+﻿public abstract class Animal
+{
+    abstract void SaySmth()
+    {
+        Console.WriteLine( " I`m an animal");
+    }
+}
