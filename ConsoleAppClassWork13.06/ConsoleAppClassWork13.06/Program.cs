dab090298ed92bdc2d12e1ae4085df0b48fc7563
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-06-14 19:44:57 -0500

    classWork13.06

---
+++ ConsoleAppClassWork13.06/ConsoleAppClassWork13.06/Program.cs
@@ -0,0 +1,32 @@
+﻿using ConsoleAppClassWork13._06;
+using System.Reflection;
+
+int i = 10;
+var intType = i.GetType();
+
+Console.WriteLine(intType.DeclaringType);
+Console.WriteLine(intType.BaseType);
+Console.WriteLine(intType.FullName);
+
+Console.ReadLine();
+
+var test = new Test();
+var testType = typeof(int);
+var objFromActivator = Activator.CreateInstance(testType);
+if (testType.Name == "Test")
+{
+    var newTest = objFromActivator as Test;
+}
+//var newTest = objFromActivator as Test;
+//MethodInfo[] methodInfo = Type.GetType("ConsoleAppClassWork13.Test").
+// GetMethods(BindingFlags.Public | BindingFlags.Instance);
+//var privatMethode = Type.GetType("ConsoleAppClassWork13.Test")
+    //.GetMethods(BindingFlags.NonPublic| BindingFlags.Instance);
+Console.WriteLine(testType.DeclaringType);
+Console.WriteLine(testType.BaseType);
+Console.WriteLine(testType.FullName);
+
+Console.ReadLine();
+
+
+
---
+++ ConsoleAppClassWork13.06/ConsoleAppClassWork13.06/Test.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClassWork13._06
+{
+    public class DefaultValue : Attribute
+    {
+        public int Value{ get; set; }
+
+        public DefaultValue(int value)
+        {
+            Value = value;
+        }
+
+    }
+    public class Test
+    {
+        [DefaultValue(10)]
+        public int Value { get; set; }
+
+        [DefaultValue (120)]
+        public int AnotherValue { get; set; }   
+       
+
+        public string Info { get; set; }
+
+        public void Print () 
+        {
+            Console.WriteLine(Value);
+        }
+
+        private void Print (int a)
+        {
+            Console.WriteLine(Info);
+        }
+    }
+}
