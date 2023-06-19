888fcfcc9a5ce52489bf4cc79f5c6461b6a544a0
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-06-18 22:03:03 -0500

    Snake

---
+++ ConsoleAppSnake/ConsoleAppSnake/Pixel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ConsoleAppSnake
+{
+    public struct  Pixel 
+    {
+        
+        public int X { get; }
+        public int Y { get; }
+        public ConsoleColor Color { get; }
+        public int PixelSize { get; }
+
+        private const char PixelChar = '█';
+
+        public Pixel(int x, int y, ConsoleColor color, int pixelSize = 3)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+            PixelSize = pixelSize;
+        }
+
+        public void PixelDraw ()
+        {
+            Console.ForegroundColor = Color;
+            Console.SetCursorPosition(X,Y);
+            Console.Write(PixelChar);
+        }
+
+        public void PixelClear()
+        {
+            Console.SetCursorPosition(X, Y);
+            Console.Write(' ');
+        }
+
+        //public Pixel GetFood(Snake snake)
+        //{
+        //    Pixel food;
+        //    do
+        //    {
+        //        food = new Pixel(Random.Next(1, MapWidth - 1), Random.Next(1, MapHeight - 1), FoodColor);
+        //    }
+        //    while (snake.Head.X == food.X && snake.Head.Y == food.Y || snake.Body.Any(b => b.X == food.X && b.Y == food.Y));
+
+        //    return food;
+        //}
+    }
+}
---
+++ ConsoleAppSnake/ConsoleAppSnake/Snake.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSnake
+{
+    public class Snake
+    {
+
+        public Pixel Head { get ; set; }
+
+        public Queue<Pixel> Body { get; } = new Queue<Pixel>();
+        public ConsoleColor HeadColor { get; set; }
+
+        public ConsoleColor BodyColor { get; set; }
+
+        public Snake (int initialX, int initialY, ConsoleColor headColor, ConsoleColor bodyColor, int bodyLength = 3)
+        {
+            HeadColor = headColor;
+            BodyColor = bodyColor;
+
+            Head = new Pixel (initialX, initialY, headColor);
+            for (int i = bodyLength; i >= 0; i--) 
+            {
+                Body.Enqueue(new Pixel(Head.X-i-1, initialY, bodyColor));
+            }
+
+            Draw();
+        }
+
+        public void Draw()
+        {
+            Head.PixelDraw();
+
+            foreach (var pixel in Body) 
+            { 
+                pixel.PixelDraw();
+            }
+        }
+
+        public void Clear()
+        {
+            Head.PixelClear();
+
+            foreach (var pixel in Body)
+            {
+                pixel.PixelClear();
+            }
+        }
+
+        public void Move(Directions direction, bool eat = false)
+        {
+            Clear();
+
+            Body.Enqueue(new Pixel(Head.X, Head.Y, BodyColor));
+
+            if (!eat)
+
+            Body.Dequeue();
+
+            Head = direction switch
+            {
+                Directions.Right => new Pixel(Head.X + 1, Head.Y, HeadColor),
+                Directions.Left => new Pixel(Head.X - 1, Head.Y, HeadColor),
+                Directions.Up => new Pixel(Head.X, Head.Y - 1, HeadColor),
+                Directions.Down => new Pixel(Head.X, Head.Y + 1, HeadColor),_=> Head
+            };
+            Draw();
+        }
+    }
+
+}
