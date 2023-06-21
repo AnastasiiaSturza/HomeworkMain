571c99871dd345a0b8438b6e4f3a7d8a99dda289
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-06-21 18:17:40 -0500

    classWork20.06

---
+++ ConsoleAppClassWork20.06/ConsoleAppClassWork20.06/Adress.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppClassWork20._06
+{
+    public class Adress
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+
+        private string State { get; set; }
+    }
+}
---
+++ ConsoleAppClassWork20.06/ConsoleAppClassWork20.06/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleAppClassWork20._06;
+using System.IO;
+using System.Xml.Serialization;
+
+XmlSerializer serrializer = new XmlSerializer(typeof(Person));
+var xmlFileName = "xmlFile.xml";
+var person = new Person
+{
+    Name = "Andriy",
+    Age = 22
+};
+List<Person> list = new List<Person>();
+list.Add(person);
+list.Add(person);
+
+
+
+using (var streamWriter = new StreamWriter(xmlFileName))
+{
+    serrializer.Serialize(streamWriter, list);
+}
+
+using (var streamWriter= new StreamReader(xmlFileName))
+{
+    var deserrialPerson = serrializer.Deserialize(streamWriter);
+    Console.ReadLine();
+}
+Console.WriteLine();
