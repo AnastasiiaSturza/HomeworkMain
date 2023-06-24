9bc7b94207e7fae82cd99cd0110c8d12d9bf587b
Anastasiia <131698570+AnastasiiaSturza@users.noreply.github.com>
2023-06-24 02:27:20 -0500

    homework

--- ConsoleAppHomeworkLinq/ConsoleAppHomeworkLinq/Program.cs
+++ ConsoleAppHomeworkLinq/ConsoleAppHomeworkLinq/Program.cs
@@ -14,9 +14,23 @@ using System.Net.Http.Json;
 //    Person person = await JsonSerializer.Deserialize<Person>(fs);
 var persons = JsonConvert.DeserializeObject<List<Person>>
               (File.ReadAllText(@"C:\Users\tuc0d\Downloads\LinqLesson\LinqLesson\data.json"));
-foreach (var person in persons)
-{
-    Console.WriteLine($"{person.Name} and {person.Id}");
+//foreach (var person in persons)
+//{
+//    Console.WriteLine($"full name : {person.Name}, eyes color: {person.About}");
+//}
+
+//var result = from p in persons where p.About == p.About select p;
+
+//var result2 = persons.Where(p => p.About == p.About);
+
+//var result3 = from person in persons from aboutPerson in person.About where person.About == person.About select person;
+
+var result = from person in persons where person.About== person.About select person;
+var result1 = persons.Where(x => persons.Select(y=> y.About).Contains(x.About));
+foreach (var p in result1)
+{ 
+   Console.WriteLine($"{p.Name}  {p.About}");
+    
 }
 
 
